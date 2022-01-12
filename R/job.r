7773d031dccfbd34e3e249ff5360e9e2b96840cf
Matt <matthewroumaya@gmail.com>
2022-01-12 09:04:18 -0500

    update job.r

--- R/job.r
+++ R/job.r
@@ -1,1 +1,12 @@
-scrape()
+mufon <- rvest::read_html(
+  'https://mufoncms.com/last_20_report_public.html'
+) %>%
+  rvest::html_element("table") %>%
+  rvest::html_table() %>%
+  dplyr::bind_rows()
+
+# rename the columns
+names(mufon) <- paste(mufon[1, ], sep = "")
+mufon <- mufon[-1,]
+
+save(mufon, file = paste0("data_raw/data_", make.names(Sys.time()), ".Rda"))
