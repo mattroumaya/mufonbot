df10e3d76878e734f3ffc7c573d2ab7eedaf88d4
mattroumaya <matthewroumaya@gmail.com>
2022-04-22 20:42:41 -0400

    remove broken url

--- job.r
+++ job.r
@@ -4,39 +4,24 @@ library(rvest)
 # this url that contained media is now blocked:
 # - "https://mufoncms.com/cgi-bin/report_handler.pl?req=latest_reports"
 
-urls <- c(
-  "https://mufoncms.com/last_20_report.html",
-  "https://mufoncms.com/last_20_report_public.html"
-)
-
-
 scrape_mufon <- function(url) {
   # scrape data
-  data <- read_html(
-    url
-  ) %>%
-    html_element(
-      "table"
-    ) %>%
+  data <- read_html(url) %>%
+    html_element("table") %>%
     rvest::html_table() %>%
     dplyr::bind_rows()
 
   names(data) <- paste(data[1, ], sep = "")
   data <- data[-1, ]
   data[["Long Description"]] <- NULL
+  data <- data %>%
+    filter(`Short Description` != "")
 
   return(data)
-
 }
 
-mufon <- purrr::map_df(urls, ~scrape_mufon(.)) %>%
-  filter(`Short Description` != "")
+mufon <- scrape_mufon("https://mufoncms.com/last_20_report_public.html")
 
-
-write.csv(
-  mufon,
-  file = paste0("data_raw/data_", make.names(Sys.time()), ".csv"),
-  row.names = FALSE
-)
+write.csv(mufon, file = paste0("data_raw/data_", make.names(Sys.time()), ".csv"), row.names = FALSE)
 
 
